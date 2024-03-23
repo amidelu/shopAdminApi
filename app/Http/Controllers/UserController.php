d4bcf8225bab1924ed9a12638e8c4ad09a7d559d
amidelu <delowar_gtc@yahoo.com>
2024-03-24 00:35:39 +0600

    register and login api complete

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class UserController extends Controller
+{
+    /**
+     * Create User
+     * @param Request $request
+     * @return User
+     */
+    public function createUser(Request $request)
+    {
+        try {
+            //Validated
+            $validateUser = Validator::make($request->all(),
+            [
+                'avatar' => 'required',
+                'type' => 'required',
+                'open_id' => 'required',
+                'name' => 'required',
+                'email' => 'required|email|unique:users,email',
+            ]);
+
+            if($validateUser->fails()){
+                return response()->json([
+                    'status' => false,
+                    'message' => 'validation error',
+                    'errors' => $validateUser->errors()
+                ], 401);
+            }
+
+            // validated will have all user fields value
+            // we can save to database
+            $validated = $validateUser -> validated();
+
+            $map = [];
+            // email, google, facebook, apple type
+            $map['type'] = $validated['type'];
+            $map['open_id'] = $validated['open_id'];
+
+            $user = User::where($map) -> first();
+
+            // Whether user has already logged in or not
+            // Empty means doen't exist
+            // Then save the user in the database for the first time
+            if (empty(user -> id)) {
+                // This token is user id
+                $validated['token'] = md5(uniquid().rand(10000, 99999));
+                // User first time created
+                $validated['created_at'] = Carbon::now();
+                $userId = User::insertGetId($validated);
+            }
+
+            $user = User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => Hash::make($request->password)
+            ]);
+
+            return response()->json([
+                'status' => true,
+                'message' => 'User Created Successfully',
+                'token' => $user->createToken("API TOKEN")->plainTextToken
+            ], 200);
+
+        } catch (\Throwable $th) {
+            return response()->json([
+                'status' => false,
+                'message' => $th->getMessage()
+            ], 500);
+        }
+    }
+
+    /**
+     * Login The User
+     * @param Request $request
+     * @return User
+     */
+    public function loginUser(Request $request)
+    {
+        try {
+            $validateUser = Validator::make($request->all(),
+            [
+                'email' => 'required|email',
+                'password' => 'required'
+            ]);
+
+            if($validateUser->fails()){
+                return response()->json([
+                    'status' => false,
+                    'message' => 'validation error',
+                    'errors' => $validateUser->errors()
+                ], 401);
+            }
+
+            if(!Auth::attempt($request->only(['email', 'password']))){
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Email & Password does not match with our record.',
+                ], 401);
+            }
+
+            $user = User::where('email', $request->email)->first();
+
+            return response()->json([
+                'status' => true,
+                'message' => 'User Logged In Successfully',
+                'token' => $user->createToken("API TOKEN")->plainTextToken
+            ], 200);
+
+        } catch (\Throwable $th) {
+            return response()->json([
+                'status' => false,
+                'message' => $th->getMessage()
+            ], 500);
+        }
+    }
+}
