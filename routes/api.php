a90b67f6896a5f51dfd3c0f5cc73895ac0e822c3
amidelu <delowar_gtc@yahoo.com>
2024-04-30 01:23:03 +0600

    route namespace problem solved

--- routes/api.php
+++ routes/api.php
@@ -3,15 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-
-
-// Route::post('/login', [UserController::class, 'createUser']);
-// // Authentication Middleware
-// Route::group(['middleware'=> ['auth:sanctum']], function () {
-//     Route::any('/courseList', [CourseController::class,'courseList']);
-// });
-
-Route::group(['namespace'=>'Api'], function(){
+Route::group(['namespace' => 'App\Http\Controllers\Api'], function(){
     Route::post('/login', 'UserController@createUser');
     Route::group(['middleware'=>'auth:sanctum'], function() {
         Route::any('/courseList', 'CourseController@courseList');
